ec8b06de6423749c7c058d03ce54d3a1e38b0103
Tom Messegee <101569909+gusroboticus@users.noreply.github.com>
2022-11-19 19:48:22 -0500

    Add files via upload
    Refactored GeodeCoin.js as follows:
    (1) Improved code readability
    (2) Added Treasury account to useEffect() - api.query.system.account(treasuryAddress)
    (3) Added proposal to useEffect() -  api.query.treasury.proposals(count)
    (4) Refactored JSX into function calls
    (5) General clean-up
    
    TODO:
    (1) Add Inflation functionality
    (2) Add .map to proposals and sum over all values
    
              
    
    Signed-off-by: Tom Messegee <101569909+gusroboticus@users.noreply.github.com>

--- src/GeodeCoin.js
+++ src/GeodeCoin.js
@@ -1,103 +1,214 @@
 import React, { useEffect, useState } from 'react'
-import { Label, Modal, Card, Icon, Grid, List, Statistic } from 'semantic-ui-react'
-
+import { Table, Label, Modal, Card, Icon, Grid, List, Statistic } from 'semantic-ui-react'
 import { useSubstrateState } from './substrate-lib'
-
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-//question circle
+import { stringToU8a } from '@polkadot/util';
+import { encodeAddress } from '@polkadot/util-crypto';
 
 function Main(props) {
+  const treasuryAddress = encodeAddress(stringToU8a("modlpy/trsry".padEnd(32, '\0')))
   const { api } = useSubstrateState()
   const [geodeInfo, setGeodeInfo] = useState({})
-  const coinName = 'geode'; //public/assets/IMG_2103.jpeg
-  const assetsMintedInB =    1000000 ;
-  const assetsAvailableInB = 100000 ;
-  const inflationRate = 'Inflation Rate:' + '4.5%' ;
-
-  
-  //const percentOfIssue = 85
+  const coinName = 'gropo'; 
+  const shortCoinName = 'g'
+  const coinFraction ='milli-'
+  const inflationRate = 'Inflation Rate: 4.5%' ;
+  const treasuryRef = 'available from Treasury';
   const infoIcon = ' Funds collected through a portion of block production rewards, \ntransaction fees, slashing, staking inefficiencies, etc. '
-
+  //const [freeJson, setFreeJason] = useState({});
   useEffect(() => {
     const getInfo = async () => {
       try {
-        const [proposalCount, approvals, totalIssuance] = await Promise.all([
+        const [proposalCount, approvals, totalIssuance, balance, proposals] = await Promise.all([
           api.query.treasury.proposalCount(),
           api.query.treasury.approvals(), 
           api.query.balances.totalIssuance(),
-        ])
-//        const [chain, nodeName, nodeVersion] = await Promise.all([
-//          api.rpc.system.chain(),
-//          api.rpc.system.name(),
-//          api.rpc.system.version(),
-//        ])
-
-        setGeodeInfo({ proposalCount, approvals, totalIssuance})
+          api.query.system.account(treasuryAddress),
+          api.query.treasury.proposals(1)
+          //api.query.balances.account('5EYCAe5ijiYfyeZ2JJCGq56LmPyNRAKzpG4QkoQkkQNB5e6Z')
+      ])
+        setGeodeInfo({ proposalCount, approvals, totalIssuance, balance, proposals })
+        
       } catch (e) {
         console.error(e)
       }
     }
+
     getInfo()
-  }, [])
+  })
 
   const propCount = JSON.stringify(geodeInfo.proposalCount);
-  //const approves = JSON.stringify(geodeInfo.approvals);
-  //const txapproves = ('🌀 No. of Approvals:' + approves).replace("[]", " None");
+  const approves = geodeInfo.approvals + '\n';
+  const totalIssue = geodeInfo.totalIssuance + '\n'; //'12000000000000000' 
+  
+  const assetsMintedInB =    totalIssue/1000000000000000000 ;
+  const totalMint =  '💰 Minted: ' + assetsMintedInB + 'B';
 
-    
-   // const totalIssue = 'Max: ' + geodeInfo.totalIssuance + '\n'; //'12000000000000000' 
-    const totalMint =  'Total Minted: ' + assetsMintedInB + 'B';
-    const percentOfIssue = 100 * (assetsAvailableInB / assetsMintedInB) ; 
+function TotalTreasury() {
+  let freeBalance = 0 
+  try {
+    freeBalance = geodeInfo.balance.data.free.toHuman().substring(0,5);
+  } catch(e) {
+    console.error(e)
+  }
+  return (
+    <div>
+      {freeBalance} K {coinName} <br></br>
+    </div>
+  )
+}  
+
+function PercentTotalIssue() {
+// Formats for JSON data --
+// propData = {"proposer":"5ECBkFwiqyQ6PJLMbcfTrRkGfijm9y1LgXc1vWPwTce3tpjE","value":"0x00000000000000000de0b6b3a7640000","beneficiary":"5DFBnZ5oHDxQbvf8vE8LjGHB4y272VmaF7DuqBR81qZFkkJM","bond":"0x000000000000000000b1a2bc2ec50000"}
+// JSONdata = {"nonce":0,"consumers":0,"providers":1,"sufficients":0,"data":{"free":"0x000000000000000036ad1f87299cb7da","reserved":0,"miscFrozen":0,"feeFrozen":0}};
+const txstrOne = "Proposed";
+const txstrTwo = "Bonded";
+const txErrorMessage ="Data Unavailable";
+try {
+    const propData=JSON.parse(geodeInfo.proposals);
+    const JSONdata=JSON.parse(geodeInfo.balance);
+    const freeBalance = JSONdata.data.free
+    const intFreeBalance=Math.round(freeBalance/1000000000);
+    const totalProposals = String(propData.value);
+    const intTotalProposals = Math.round(totalProposals/1000000000);
+    const intTotalBond = Math.round(propData.bond/1000000000)
+    const percentOfIssue = intFreeBalance > 0 ? Math.round(100 * (intTotalProposals / intFreeBalance)) : 0; 
+
+  return(
+    <div>
+      <Table>
+        <Table.Row>
+          <Table.Cell >
+    			     Percent of Treasury Proposed: 
+    			    <div style={{ width: 50, height: 50 }}>
+      		    <CircularProgressbar value={percentOfIssue} text= {`${percentOfIssue}%`} strokeWidth={12}/>
+    			    </div>
+		      </Table.Cell>
+          <Table.Cell>
+          <Label ribbon color='teal' style={{ width: 100}}>{txstrOne}</Label> <br></br>
+            <strong> {intTotalProposals + shortCoinName} </strong><br></br>
+          <Label ribbon color='blue' style={{ width: 100}}>{txstrTwo}  </Label> <br></br>
+			      <strong>{intTotalBond + shortCoinName}</strong><br></br>
+		        </Table.Cell>
+        </Table.Row>
+      </Table>
+  </div>
+  )
+} catch(e) {
+  console.error(e)
+  return (
+    <div>{txErrorMessage}</div>
+  )
+}
+}
+
+function TreasuryBalances() {
+try {  
+  const freeBalance = geodeInfo.balance.data.free.toHuman(); 
+  const reservedBalance = JSON.stringify(geodeInfo.balance.data.reserved); 
+  const miscFrozenBalance = JSON.stringify(geodeInfo.balance.data.miscFrozen); 
+  const feeFrozenBalance = JSON.stringify(geodeInfo.balance.data.feeFrozen); 
+  return (
+    <div>
+      🔹 Total free: <strong>{freeBalance}</strong> {coinName} <br></br>
+      🔹 Reserved: <strong>{reservedBalance}</strong> {coinName} <br></br>
+      🔹 Misc Frozen: <strong>{miscFrozenBalance}</strong> {coinName} <br></br>
+      🔹 Fee Frozen: <strong>{feeFrozenBalance}</strong> {coinName} <br></br>
+      <br></br>
+    </div>
+  )
+} catch(e) {
+  console.error(e)
+  return null
+}  
+}
+
+function TreasuryDescription() {
+try {
+  return (
+    <div>
+    <strong>Treasury Information: </strong><br></br>
+    🔹 Total Issuance: <strong>{totalIssue} </strong>{coinName}<br></br>
+    🔹 Total Issuance in B {coinName}: <strong>{assetsMintedInB} </strong><br></br>
+    🔹 Burn Amount: <strong>{api.consts.treasury.burn.toString()}</strong> {coinName}- Percentage of spare funds (if any) that are burnt per spend period <br></br>
+    🔹 Spend Period: <strong>{api.consts.treasury.spendPeriod.toString()}</strong> - in Blocks, period between successive spends.<br></br>
+    🔹 Proposal Count: <strong>{propCount}</strong> - Number of current proposals <br></br>
+    🔹 Max Approvals in Spending Queue: <strong>{api.consts.treasury.maxApprovals.toString()}</strong> - The maximum number of approvals that can wait in the spending queue.<br></br>
+    🔹 Number of Approved Proposals: <strong></strong>{approves} <br></br>
+    🔹 Max Proposal Bond: <strong>{api.consts.treasury.proposalBondMaximum.toString()}</strong> - Maximum amount of funds in {coinName} that should be placed in a deposit for making a proposal.<br></br>
+    🔹 Min Proposal Bond: <strong>{api.consts.treasury.proposalBondMinimum.toString()}</strong> - Minimum amount of funds in {coinFraction} {coinName} that should be placed in a deposit for making a proposal.<br></br>
+    🔹 Proposal Bond: <strong>{api.consts.treasury.proposalBond.toString()}</strong> {coinName} - Fraction of a proposal's value that should be bonded in order to place the proposal. <br></br>
+    An accepted proposal gets these back. A rejected proposal does not.<br></br>
+    <br></br>
+    </div>
+  )
+} catch(e) {
+  console.error(e)
+  return null
+}
+}
+
+function StakingDescription() {
+  try {
+    return (
+      <div>
+      <strong>Staking Information:</strong><br></br>
+      🔹 Bonding Duration: <strong>{api.consts.staking.bondingDuration.toString()}</strong> - Number of eras that staked funds must remain bonded for.<br></br>
+      🔹 Max Nominations: <strong>{api.consts.staking.maxNominations.toString()}</strong> - Maximum number of nominations per nominator.<br></br>
+      🔹 Max Nominations Rewarded Per Validator: <strong>{api.consts.staking.maxNominatorRewardedPerValidator.toString()}</strong> - The maximum number of nominators rewarded for each validator. <br></br>
+      🔹 Sessions per Era: <strong>{api.consts.staking.sessionsPerEra.toString()}</strong> - Number of sessions per era. <br></br>
+      🔹 Slash Defer Duration: <strong>{api.consts.staking.slashDeferDuration.toString()}</strong> - Number of eras that slashes are deferred by, after computation. <br></br>
+      🔹 Eras of Staker Unbonding: <strong>{api.consts.staking.maxUnlockingChunks.toString()}</strong> - How many unique eras a staker may be unbonding in. <br></br>
+      <br></br>                            
+      </div>
+    )
+  } catch(e) {
+    console.error(e)
+    return null
+  }
+}
 
   return (
     <Grid.Column>
       <Card color='teal'>
         <Card.Content>
         <Card.Meta>
-            <span> Available </span>       
         <Modal trigger={<Icon link name="info circle" />}>
-                    <Modal.Header>Information - Available Geode</Modal.Header>
-                    <Modal.Content scrolling wrapped>
-                    <Modal.Description>
-                           <Label color='teal'> available Geode </Label>  <br></br>                           
+                    <Modal.Header>Information - Available Geode in Treasury</Modal.Header>
+                    <Modal.Content scrolling wrapped="true">
+                    <Modal.Description>                   
+                           <Label color='teal'> Available Geode - Treasury</Label>  <br></br>                           
                            {infoIcon} <br></br><br></br>
-                           <Label color='teal'> treasuryInfo </Label>  <br></br>
-                           <strong>Treasury Information: </strong><br></br>
-                           Max Proposal Bond: {0}<br></br>
-                           Min Proposal Bond: {0}<br></br>
-                           Proposal Bond: <br></br>
-                           Burn Amount:<br></br>
-                           Max Approvals: {0}<br></br>
-
+                           💰 Treasury Address: <strong>{treasuryAddress.toString()} </strong><br></br>
                            <br></br>
+                           <strong>Treasury:</strong><br></br>
+                            <TreasuryBalances />
+                            <TreasuryDescription />
+                            <StakingDescription />
                     </Modal.Description>
                     </Modal.Content>
                 </Modal>
+                <span> Available </span>  <br></br>     
           </Card.Meta>
           <Card.Header> 
           <Statistic size='tiny'>
-                <Statistic.Label></Statistic.Label>
-                <Statistic.Value>{assetsAvailableInB} B {coinName} </Statistic.Value>
-                </Statistic>    
-               </Card.Header>
-                 available from Treasury
+              <Statistic.Label></Statistic.Label>
+              <Statistic.Value><TotalTreasury /></Statistic.Value>
+          </Statistic>    
+          </Card.Header>
+              {treasuryRef}                 
           <Card.Description> 
-
-          <strong> % of Total Issued: </strong>
-                    <div style={{ width: 50, height: 50 }}>
-                        <CircularProgressbar value={percentOfIssue} text= {`${percentOfIssue}%`} strokeWidth={12}/>
-                    </div>
-
-             </Card.Description>
+          <PercentTotalIssue />
+          </Card.Description>
         </Card.Content>
-        <Label Basic ><weak>{inflationRate}</weak></Label>
+        <Label >{inflationRate}</Label>
         <Card.Content extra>
         <List>
         <List.Item>{totalMint}</List.Item>
         <List.Item>🌀 No. of Proposals: {propCount} </List.Item>
-           
-            </List>
+        </List>
         </Card.Content>
       </Card>
     </Grid.Column>
@@ -109,7 +220,9 @@ export default function GeodeCoin(props) {
   return api.query &&
   api.query.treasury &&
   api.query.treasury.proposalCount &&
-  api.query.treasury.approvals ? (
+  api.query.treasury.approvals &&
+  api.query.balances.totalIssuance &&
+  api.query.balances.account ? (
     <Main {...props} />
   ) : null
 }
