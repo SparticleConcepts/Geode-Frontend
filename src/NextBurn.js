dd6f5bd6ba37b363faf37b9ea0af34e435a019d8
Tom Messegee <101569909+gusroboticus@users.noreply.github.com>
2022-11-30 11:26:22 -0500

    Add files via upload
    CARD 4 NextBurn.js
    ℹ️ Completed refactoring and hooks for interfaces to the GROPO test chain.
    ℹ️ Tested 👍
    
    ACTIONS:
    1. Need to determine difference between spendable/available
    2. Check info and update the info modal
    
    Updates to Card:
    Next Burn...
    burn: Permill
    interface: api.consts.treasury.burn
    summary: Percentage of spare funds (if any) that are burnt per spend period. This percentage should look like 1%.
    Calculate NextBurn = 0.01*TreasuryBalance
    % of Treasury ...
    burn: Permill
    interface: api.consts.treasury.burn
    summary: Percentage of spare funds (if any) that are burnt per spend period. Should be a consistent 1%.
    Treasury After Burn ...
             Calculate TreasuryAfterBurn = TreasuryBalance - NextBurn
     Ideal Staked % ...
    the method for inflation is written in frame/staking/reward-curve and is initialized in bin/node/runtime/lib.rs
    It will look like this:
    ideal_stake: 0_500_000
    Which means 50% (remove 4 zeros)
    I recommend hard coding that to the standard in our chains which should be 50% as it never changes.
    Total Staked % ...
    erasTotalStake(u32): u128
    interface: api.query.staking.erasTotalStake
    summary: The total amount staked for the last HISTORY_DEPTH eras. If total hasn't been set or has been removed then 0 stake is returned.
    Calculate TotalStakedPercent = erasTotalSTake (for 1 era) / TotalIssuance
    
    Signed-off-by: Tom Messegee <101569909+gusroboticus@users.noreply.github.com>

--- src/NextBurn.js
+++ src/NextBurn.js
@@ -1,82 +1,206 @@
-import React, { useEffect, useState } from 'react'
-import { Label, Icon, Modal, Card, Grid, List, Statistic } from 'semantic-ui-react'
-
+import React, { useEffect, useState, useRef } from 'react'
+import { Table, Label, Icon, Modal, Card, Grid, Statistic } from 'semantic-ui-react'
 import { useSubstrateState } from './substrate-lib'
-
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { stringToU8a } from '@polkadot/util';
+import { encodeAddress } from '@polkadot/util-crypto';
 
 
 function Main(props) {
+  const treasuryAddress = encodeAddress(stringToU8a("modlpy/trsry".padEnd(32, '\0')))
   const { api } = useSubstrateState()
   const [geodeInfo, setGeodeInfo] = useState({})
-  
-  const burnAmount = 349.2 ; 
-  const treasuryBeforeBurn = 100000 ;
-  const treasuryAfterBurn = ( ( ( 1000 * treasuryBeforeBurn ) - burnAmount) / 1000 )
-  const coinName = 'geode'; //public/assets/IMG_2103.jpeg
+  const idealStaked = '50%';
+  const prevEra = useRef(482)
 
-  const percentOfTreasury = 5
-  const infoIcon = 'If the Treasury ends a spend period without spending all of its funds, \nit suffers a burn of a percentage of its funds.'
-  //const inflationRate = 'Inflation Rate:' + '4.5%' ;
-  
   useEffect(() => {
     const getInfo = async () => {
       try {
-        const [chain, nodeName, nodeVersion] = await Promise.all([
+        const [chain, era, staked, balance, totalIssuance] = await Promise.all([
           api.rpc.system.chain(),
-          api.rpc.system.name(),
-          api.rpc.system.version(),
+          api.query.staking.activeEra(),
+          api.query.staking.erasTotalStake(prevEra.current),
+          api.query.system.account(treasuryAddress),
+          api.query.balances.totalIssuance(),
         ])
 
-        setGeodeInfo({ chain, nodeName, nodeVersion})
+        setGeodeInfo({ chain, era, staked, balance, totalIssuance})
       } catch (e) {
         console.error(e)
       }
     }
     getInfo()
-  }, [api.rpc.system])
+  }, )
+
+  function Token() {
+    const errorMessage = 'unavailable';
+    let coinName = 'Coin'; //public/assets/IMG_2103.jpeg
+    try {
+      coinName = geodeInfo.chain.toString()
+      return (
+        <>
+          {coinName}
+        </>
+      )
+    } catch(e) {
+      console.error(e)
+      return(
+          <>
+          {errorMessage}<br></br>
+          </>
+      )
+    }
+  }
+
+  function NextTreasuryBurn() {
+    const errorMessage = 'Burn Data not Available';
+    let freeBalance = 0 
+    let burnAmount = 9999 ; 
+    try {
+      const burnPercent = (api.consts.treasury.burn.toString())/1000000;  
+      freeBalance = geodeInfo.balance.data.free.toString() //toHuman();
+      burnAmount = ((burnPercent * freeBalance / 1000000000) /1000000).toString().substring(0,6)
+      return (
+        <>
+          {burnAmount} K <Token /> <br></br>
+        </>
+      )
+      } catch(e) {
+      console.error(e)
+      return (
+        <>
+          {errorMessage}<br></br>
+        </>
+      )
+      }
+  }  
+
+  function TotalTreasuryAfterBurn() {
+    let treasuryBalance = 0; 
+    let treasuryAfterBurn = 0;
+    try {
+      const burnPercent = (api.consts.treasury.burn.toString())/1000000;  
+      treasuryBalance = geodeInfo.balance.data.free.toString() //toHuman();
+      const burnAmount = (burnPercent * treasuryBalance)
+      treasuryAfterBurn = (( (treasuryBalance - burnAmount) / 1000000000 ) / 1000000).toString().substring(0,9)
+    } catch(e) {
+      console.error(e)
+    }
+    return (
+      <>
+       <Label basic >After Burn: {treasuryAfterBurn} K <Token /></Label>
+      </>
+    )
+  }
+
+  function PercentSpenableAvailable() {
+    const txErrorMessage ="Data Unavailable";
+    let treasuryBalance = 0; 
+    try {
+      const burnPercent = (api.consts.treasury.burn.toString())/10000;  
+      treasuryBalance = (.000000001 * (geodeInfo.balance.data.free.toString())/1000000000).toString().substring(0,6) //toHuman();
+    return(
+          <div>
+            <Table>
+              <Table.Row>
+                <Table.Cell >
+                      🔹 Spendable: <strong>{treasuryBalance} M <Token /> </strong><br></br>
+                      🔹 Available: <strong>{treasuryBalance} M <Token /> </strong><br></br>
+                      🔹 % Burn of Treasury
+                      <div style={{ width: 50, height: 50 }}>
+                          <CircularProgressbar value={burnPercent} text= {`${burnPercent}%`} strokeWidth={12}/>
+                      </div>
+                </Table.Cell>
+              </Table.Row>
+            </Table>
+        </div>
+        )
+      } catch(e) {
+        console.error(e)
+        return (
+          <div>{txErrorMessage}</div>
+        )
+      }
+      }
+  
+  function StakingInfo() {
+        const errorMessage = 'Burn Data not Available';
+        let totalStaked = 9999 ; 
+        let totalIssue = 9999 ;
+        let stakedPercent = 0;
+        try {
+          const newEraJSON = JSON.parse(geodeInfo.era);
+          const currentEra = String(newEraJSON.index);
+          totalStaked = Math.round((geodeInfo.staked.toString())/1000000000);
+          totalIssue = Math.round((geodeInfo.totalIssuance.toString())/1000000000);
+          stakedPercent = (100 * totalStaked/totalIssue).toString().substring(0,5);
+          prevEra.current = currentEra;
+          return (
+            <>
+              % Ideal Staked: {idealStaked} <br></br>
+              % Staked: {stakedPercent}% @ Era: {currentEra} <br></br>
+            </>
+          )
+          } catch(e) {
+          console.error(e)
+          return (
+            <>
+              {errorMessage}<br></br>
+            </>
+          )}
+  }  
+  
+  function NextBurnModal() {
+    const infoIcon = 'If the Treasury ends a spend period without spending all of its funds, \nit suffers a burn of a percentage of its funds.'
+    return(
+      <>
+      <strong><Icon name="info circle" />Next Burn Information: </strong><br></br>
+      <Icon name="toggle on" /> Next Burn: {infoIcon} <br></br>
+      <Icon name="toggle on" /> Next Burn Total: Total to be burned after the spend period. <br></br>
+      <Icon name="toggle on" /> Spendable: Total amount in Treasury that is spendable. <br></br>
+      <Icon name="toggle on" /> Available: Total amount in Treasury that is available for use. <br></br>
+      <Icon name="toggle on" /> % Burn of Treasury: The percent of the Treasury that will burned at each spend period. <br></br>
+      <Icon name="toggle on" /> After Burn: The amount left in Treasury after the current period is completed.<br></br>
+      <Icon name="toggle on" /> % Ideal Staked: Ideal Amount staked in the system. Total Staked / Total Issuance <br></br>
+      <Icon name="toggle on" /> % Staked: The current ammount in the Era staked. Total Staked / Total Issuance <br></br>
+      </>
+    )
+  }
+
+
 
   return (
     <Grid.Column>
       <Card color='teal'>
         <Card.Content>
         <Card.Meta>
-            <span> Next Burn </span>
             <Modal trigger={<Icon link name="info circle" />}>
                     <Modal.Header>Information - Next Burn</Modal.Header>
-                    <Modal.Content scrolling wrapped>
+                    <Modal.Content scrolling wrapped="true">
                     <Modal.Description>
-                    <pre>
-                           <code>                             
-                           {infoIcon} <br></br>
-                            </code>
-                    </pre>
+                      <NextBurnModal />     
                     </Modal.Description>
                     </Modal.Content>
                 </Modal>
+                <span> Next Burn </span>
           </Card.Meta>
           <Card.Header> 
           <Statistic size='tiny'>
                 <Statistic.Label></Statistic.Label>
-                <Statistic.Value>{burnAmount} {coinName} </Statistic.Value>
+                <Statistic.Value>
+                <NextTreasuryBurn />
+                 </Statistic.Value>
                 </Statistic>    
                </Card.Header>
                Total
           <Card.Description> 
-          <strong> % of Treasury</strong>
-                    <div style={{ width: 50, height: 50 }}>
-                        <CircularProgressbar value={percentOfTreasury} text= {`${percentOfTreasury}%`} strokeWidth={12}/>
-                    </div>
-
-             </Card.Description>
+          <PercentSpenableAvailable />
+        </Card.Description>
         </Card.Content>
-        <Label Basic ><weak>Treasury After Burn: {treasuryAfterBurn} M</weak></Label>
+        <TotalTreasuryAfterBurn />
         <Card.Content extra>
-        <List>
-            <List.Item>🌀 Ideal Staked: {geodeInfo.chain} </List.Item>
-            <List.Item>🌀 Staked: {geodeInfo.nodeName}</List.Item>
-            </List>
+        <StakingInfo />        
         </Card.Content>
       </Card>
     </Grid.Column>
@@ -88,8 +212,15 @@ export default function NodeInfo(props) {
   return api.rpc &&
     api.rpc.system &&
     api.rpc.system.chain &&
-    api.rpc.system.name &&
-    api.rpc.system.version ? (
+    api.query.staking.activeEra &&
+    api.query.staking.erasTotalStake &&
+    api.query.system.account &&
+    api.query.balances.totalIssuance ? (
     <Main {...props} />
   ) : null
 }
+// api.rpc.system.chain(),
+// api.query.staking.activeEra(),
+// api.query.staking.erasTotalStake(prevEra.current),
+// api.query.system.account(treasuryAddress),
+// api.query.balances.totalIssuance(),
